af2c72a1e11bef34926fd8a80ec39ee167cbda7f
chivinh <chivinhly.kul@gmail.com>
2019-09-12 16:39:02 +0700

    testmiddle

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <a href="/">Trang chu</a>
+                    <a href="phimthieunhi">Phim thieu nhi</a>
+                    <a href="phimkiemhiep">Phim kiem hiep</a>
+                    <a href="phimtinhcam">Phim tinh cam</a>
+                    <a href="phim16+">Phim 16+</a>
+                </div>
+                <div class="col-1"></div>
+            </div>
+</div>
+@endsection
